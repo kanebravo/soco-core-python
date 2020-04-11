0cfa3e4b52208a5a261fe86319ef9a5db5838535
kanebravo <63410858+kanebravo@users.noreply.github.com>
2020-04-11 13:57:41 +0200

    Update quick_start.py

--- quick_start.py
+++ quick_start.py
@@ -2,7 +2,7 @@ from soco_core.soco_client import SOCOClient
 from soco_core.examples import load_example_doc_data
 
 if __name__ == '__main__':
-    ADMIN_API_KEY = '898706a0-ecb2-457d-8f89-eea1c406f0ca'
+    ADMIN_API_KEY = '965c966a-07db-4f3b-8f26-56293b21c3c9'
     a_client = SOCOClient(ADMIN_API_KEY)
 
     # add data
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     a_client.publish()
 
     print("Make a query")
-    QUERY_API_KEY = '727bb6b3-455c-4ee5-8f48-c2ab95837e56'
+    QUERY_API_KEY = '8228868a-4b50-4b4b-8a1c-8a010122cf8b'
     q_client = SOCOClient(QUERY_API_KEY)
     responses = q_client.query({"query": "what is the distance from earth to sun?", "n_best": 10})
     SOCOClient.pprint(responses)
